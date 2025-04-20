328feddfa857df09dc27f87f458eef2938e0a95f
DHRUMIL PATEL <dpatel24@radar.gsw.edu>
2025-04-20 17:43:21 -0400

    .

--- meme_mingle/src/app/shared/environments/environment.prod.ts
+++ meme_mingle/src/app/shared/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
     baseUrl: 'https://mememinglebackend.redrock-0e165097.eastus.azurecontainerapps.io', //development backend URL
+    apiBaseUrl: 'http://localhost:5000',
   };
